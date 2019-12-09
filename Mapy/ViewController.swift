cd7e6d026ad683b978514e7c7d4f7603a3817352
Leszek Iwanowski <iwanowskileszek@gmail.com>
2019-12-09 17:30:18 +0100

    commit

--- Mapy/ViewController.swift
+++ Mapy/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var Krakow: UIButton!
+    
+    @IBOutlet weak var Zakopane: UIButton!
+    
+    @IBOutlet weak var Lodz: UIButton!
+    
+    @IBOutlet weak var Lida: UIButton!
+    
+   
+    @IBOutlet weak var hybrydowa: UIButton!
+    
+    @IBOutlet weak var satelitarna: UIButton!
+    
+    @IBOutlet weak var drogowa: UIButton!
+    
+    @IBAction func pokazKrakow(sender: AnyObject){
+        let location = CLLocationCoordinate2DMake(50.0246289, 19.8924691)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: location, span: span )
+        mapView.setRegion( region, animated: true)
+        }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+        
     }
 
 
